d2e60ffa3e1aaad7cf4c9a9c28db07f9bdf7616d
VVilhelmina <mina.milovanovic@gmail.com>
2019-03-28 11:42:14 +0100

    unwanted folder deleted

--- 15032019/15032019/IChangeable.java
+++
@@ -1,6 +0,0 @@
-package novipaket;
-
-public interface IChangeable {
-	public void changeState();
-	public void printState();
-}
--- 15032019/15032019/IMoveable.java
+++
@@ -1,9 +0,0 @@
-package novipaket;
-
-public interface IMoveable {
-	public void printLocation();
-	public void moveUp(double value);
-	public void moveDown(double value);
-	public void moveLeft(double value);
-	public void moveRight(double value);
-}
